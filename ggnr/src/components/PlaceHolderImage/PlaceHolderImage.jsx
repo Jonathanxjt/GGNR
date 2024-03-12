fbe051d132ecb397e343de6b9603fd61f92ed887
matthewaeria <matthewjamesaeria@gmail.com>
2024-03-12 15:16:02 +0800

    placeholder carousel

---
+++ ggnr/src/components/PlaceHolderImage/PlaceHolderImage.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const PlaceholderImage = () => {
+    return (
+        <div className="placeholder-image">
+            <img src="https://via.placeholder.com/300" alt="Placeholder" />
+        </div>
+    );
+};
+
+export default PlaceholderImage;
