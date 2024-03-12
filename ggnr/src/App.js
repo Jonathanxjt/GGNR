58a39ca388e4aba03d089ed37b375ad4442c8560
Jonathanxjt <59588469+Jonathanxjt@users.noreply.github.com>
2024-03-12 15:33:55 +0800

    Smol change

--- ggnr/src/App.js
+++ ggnr/src/App.js
@@ -3,22 +3,15 @@ import React from 'react';
 import {MyNavbar} from './components/MyNavbar/MyNavbar';
 import RouteConfig from './Route';
 
-
 function App() {
+
   return (
     <div className="App">
-      <Nav/>
+      <MyNavbar/>
       <RouteConfig />
     </div>
   );
 }
 // How to run components in React
-function Nav() {
-  return (
-    <div>
-      <MyNavbar/>
-    </div>
-  );
-}
 
 export default App;
