4e2b3eb2315533a78fc9a118de6aaabb4693b69e
Rexisk <darrenfoo7@gmail.com>
2024-03-16 19:30:03 +0800

    updated ticket.py

--- ggnr/microservice/ticket.py
+++ ggnr/microservice/ticket.py
@@ -13,7 +13,7 @@ import json
 
 app = Flask(__name__)
 app.config["SQLALCHEMY_DATABASE_URI"] = (
-    environ.get("dbURL") or "mysql+mysqlconnector://root@localhost:3306/ggnr"
+    environ.get("dbURL") or "mysql+mysqlconnector://root:root@localhost:3306/ggnr_database"
 )
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 
@@ -21,9 +21,37 @@ db = SQLAlchemy(app)
 
 CORS(app)
 
-class Ticket(db.Model):
-    __tablename__ = "ticket"
+class Attendee(db.Model):
+    __tablename__ = 'attendees'
+    
+    AID = db.Column(db.Integer, autoincrement=True, primary_key=True)
+    EID = db.Column(db.Integer, ForeignKey('events.EID'))
+    UID = db.Column(db.Integer, ForeignKey('users.UID'))
+    ticketID = db.Column(db.Integer)
+    transactionID = db.Column(db.Integer)
+    
+    # Relationships
+    event = relationship('Event', back_populates='attendees')
+    user = relationship('User', back_populates='attendees')
+    def __init__(self, EID, UID, ticketID, transactionID):
+        self.EID = EID
+        self.UID = UID
+        self.ticketID = ticketID
+        self.transactionID = transactionID
+    
+    def to_json(self):
+        return {
+            'AID': self.AID,
+            'EID': self.EID,
+            'UID': self.UID,
+            'ticketID': self.ticketID,
+            'transactionID': self.transactionID
+        }
+    
 
+class Ticket(db.Model):
+    __tablename__ = 'tickets'
+    
     TicketID = db.Column(db.Integer, primary_key=True)
     EID = db.Column(db.Integer, ForeignKey('events.EID'))
     UID = db.Column(db.Integer, ForeignKey('users.UID'))
@@ -47,204 +75,191 @@ class Ticket(db.Model):
             'Tier': self.Tier,
             'Price': self.Price
         }
+
+class Event(db.Model):
+    __tablename__ = 'events'
+    
+    EID = db.Column(db.Integer, primary_key=True)
+    TierID = db.Column(db.SmallInteger, primary_key=True)
+    Title = db.Column(db.String(255))
+    Description = db.Column(db.Text)
+    EventLogo = db.Column(db.Text)
+    GameName = db.Column(db.String(255))
+    GameLogo = db.Column(db.Text)
+    Location = db.Column(db.String(255))
+    Time = db.Column(db.DateTime)
+    GameCompany = db.Column(db.String(255))
+    Capacity = db.Column(db.Integer)
+    Price = db.Column(db.Float(precision=2))
+    
+    # Relationships
+    attendees = relationship('Attendee', back_populates='event')
+    tickets = relationship('Ticket', back_populates='event')
+
+    def __init__(self, EID, TierID, Title, Description, EventLogo, GameName, GameLogo, Location, Time, GameCompany, Capacity, Price):
+        self.EID = EID
+        self.TierID = TierID
+        self.Title = Title
+        self.Description = Description
+        self.EventLogo = EventLogo
+        self.GameName = GameName
+        self.GameLogo = GameLogo
+        self.Location = Location
+        self.Time = Time
+        self.GameCompany = GameCompany
+        self.Capacity = Capacity
+        self.Price = Price
+
+    def json(self):
+        return {
+            "EID": self.EID,
+            "TierID": self.TierID,
+            "Title": self.Title,
+            "Description": self.Description,
+            "EventLogo": self.EventLogo,
+            "GameName": self.GameName,
+            "GameLogo": self.GameLogo,
+            "Location": self.Location,
+            "Time": self.Time.isoformat() if self.Time else None,  # ISO formatting for dateTime
+            "GameCompany": self.GameCompany,
+            "Capacity": self.Capacity,
+            "Price": self.Price
+        }
+
+class User(db.Model):
+    __tablename__ = 'users'
+    
+    UID = db.Column(db.Integer, primary_key=True)
+    preferences = db.Column(db.Text)
+    
+    # Relationships
+    attendees = relationship('Attendee', back_populates='user')
+    tickets = relationship('Ticket', back_populates='user')
+
+    def __init__(self, UID, preferences):
+        self.UID = UID
+        self.preferences = preferences
+    
+    def to_json(self):
+        return {
+            'UID': self.UID,
+            'preferences': self.preferences
+        }
     
 @app.route("/ticket")
 def get_all():
     ticket_list = db.session.scalars(db.select(Ticket)).all()
-
     if len(ticket_list):
         return jsonify(
             {
-                "code": 201,
-                "data": [individual_ticket.json() for individual_ticket in ticket_list]
+                "code": 200,
+                "data": {
+                    "tickets": [individual_ticket.json() for individual_ticket in ticket_list]
+                }
             }
         )
     return jsonify({"code": 404, "message": "There are no tickets."}), 404
 
-@app.route("/ticket/EID/<string:EID>")
-def get_tickets_by_eid(EID):
-    ticket_list = db.session.scalars(db.select(Ticket).filter_by(EID)).all()
-    if len(ticket_list):
-        return jsonify(
-            {
-                "code": 201,
-                "data": [individual_ticket.json() for individual_ticket in ticket_list]
-            }
-        )
-    return jsonify({"code": 404, "message": "There are no tickets for this event."}), 404
-
-
-@app.route("/ticket/UID/<string:UID>")
-def get_tickets_by_uid(UID):
-    ticket_list = db.session.scalars(db.select(Ticket).filter_by(UID)).all()
-    if len(ticket_list):
-        return jsonify(
-            {
-                "code": 201,
-                "data": [individual_ticket.json() for individual_ticket in ticket_list]
-            }
-        )
-    return jsonify({"code": 404, "message": "There are no tickets for this user."}), 404
-
-# combine - UID,EID
-@app.route("/ticket/EID/UID/<string:combine>")  
-def get_ticket_by_uid_eid(combine):
-    split = combine.split(",")
-    uid = split[0]
-    eid = split[1]
-
-    ticket = db.session.scalars(db.select(Ticket).filter_by(uid).filter_by(eid).limit(1)).first()
-
-    if ticket:
-        return jsonify(
-            {
-                "code": 200,
-                "data": ticket.json()
-            }
-        )
-    
-    return jsonify(
-        {
-            "code": 404,
-            "message": "Ticket not found."
-        }
-    ), 404
-
-# @app.route("/ticket")
-# def get_all():
-
-    
-#     eid = request.get_json().get("EID")
-#     uid = request.args.get("UID")
-
-#     # get ticket by eid and uid
-#     if eid != None and uid != None:
-#         ticket = db.session.scalars(db.select(Ticket).filter_by(EID=eid).filter_by(UID=uid).limit(1)).first()
-
-#         if ticket:
-#             return jsonify(
-#                 {
-#                     "code": 200,
-#                     "data": ticket.json()
-#                 }
-#             )
-        
-#         else:
-#             return jsonify(
-#                 {
-#                     "code": 404,
-#                     "message": "Ticket not found."
-#                 }
-#             ), 404
-        
-#     if eid != None:
-#         ticket_list_by_eid = db.session.scalars(db.select(Ticket).filter_by(EID=eid)).all()
-        
-#         if len(ticket_list_by_eid):
-#             return jsonify(
-#                 {
-#                     "code": 200,
-#                     "data": {
-#                         "tickets": [individual_ticket.json() for individual_ticket in ticket_list_by_eid]
-#                     }
-#                 }
-#             )
-#         else:
-#             return jsonify(
-#                 {
-#                     "code": 404,
-#                     "message": "There are no tickets."
-#                 }
-#             ), 404
-    
-#     if uid != None:
-#         ticket_list_by_uid = db.session.scalars(db.select(Ticket).filter_by(UID=uid)).all()
-
-#         if len(ticket_list_by_uid):
-#             return jsonify(
-#                 {
-#                     "code": 200,
-#                     "data": {
-#                         "tickets": [individual_ticket.json() for individual_ticket in ticket_list_by_uid]
-#                     }
-#                 }
-#             )
-#         else:
-#             return jsonify(
-#                 {
-#                     "code": 404,
-#                     "message": "There are no tickets."
-#                 }
-#             ), 404
-
-#     ticket_list = db.session.scalars(db.select(Ticket)).all()
+# @app.route("/ticket/EID/<string:EID>")
+# def get_tickets_by_eid(EID):
+#     ticket_list = db.session.scalars(db.select(Ticket).filter_by(EID)).all()
 #     if len(ticket_list):
 #         return jsonify(
 #             {
+#                 "code": 201,
+#                 "data": [individual_ticket.json() for individual_ticket in ticket_list]
+#             }
+#         )
+#     return jsonify({"code": 404, "message": "There are no tickets for this event."}), 404
+
+
+# @app.route("/ticket/UID/<string:UID>")
+# def get_tickets_by_uid(UID):
+#     ticket_list = db.session.scalars(db.select(Ticket).filter_by(UID)).all()
+#     if len(ticket_list):
+#         return jsonify(
+#             {
+#                 "code": 201,
+#                 "data": [individual_ticket.json() for individual_ticket in ticket_list]
+#             }
+#         )
+#     return jsonify({"code": 404, "message": "There are no tickets for this user."}), 404
+
+# # combine - UID,EID
+# @app.route("/ticket/EID/UID/<string:combine>")  
+# def get_ticket_by_uid_eid(combine):
+#     split = combine.split(",")
+#     uid = split[0]
+#     eid = split[1]
+
+#     ticket = db.session.scalars(db.select(Ticket).filter_by(uid).filter_by(eid).limit(1)).first()
+
+#     if ticket:
+#         return jsonify(
+#             {
 #                 "code": 200,
-#                 "data": {
-#                     "tickets": [ticket.json() for ticket in ticket_list]
-#                 }
+#                 "data": ticket.json()
 #             }
 #         )
     
 #     return jsonify(
 #         {
 #             "code": 404,
-#             "message": "There are no tickets."
+#             "message": "Ticket not found."
 #         }
 #     ), 404
 
 
-# POST - create ticket
-@app.route("/ticket/<string:TicketID>", methods=["POST"])
-def create_ticket(TicketID):
+# # POST - create ticket
+# @app.route("/ticket/<string:TicketID>", methods=["POST"])
+# def create_ticket(TicketID):
 
-    eid = request.get_json().get("EID")
-    uid = request.get_json().get("UID")
-    tier = request.get_json().get("Tier")
-    price = request.get_json().get("Price")
+#     eid = request.get_json().get("EID")
+#     uid = request.get_json().get("UID")
+#     tier = request.get_json().get("Tier")
+#     price = request.get_json().get("Price")
 
-    ticket = Ticket(
-        TicketID,
-        eid,
-        uid,
-        tier,
-        price
-    )
+#     ticket = Ticket(
+#         TicketID,
+#         eid,
+#         uid,
+#         tier,
+#         price
+#     )
 
-    try:
-        db.session.add(ticket)
-        db.session.commit()
-    except Exception as e:
-        exc_type, exc_obj, exc_tb = sys.exc_info()
-        fname = os.path.split(exc_tb.tb_frame.f_code.co_filename)[1]
-        ex_str = (
-            str(e)
-            + " at "
-            + str(exc_type)
-            + ": "
-            + fname
-            + ": line "
-            + str(exc_tb.tb_lineno)
-        )
-        print(ex_str)
+#     try:
+#         db.session.add(ticket)
+#         db.session.commit()
+#     except Exception as e:
+#         exc_type, exc_obj, exc_tb = sys.exc_info()
+#         fname = os.path.split(exc_tb.tb_frame.f_code.co_filename)[1]
+#         ex_str = (
+#             str(e)
+#             + " at "
+#             + str(exc_type)
+#             + ": "
+#             + fname
+#             + ": line "
+#             + str(exc_tb.tb_lineno)
+#         )
+#         print(ex_str)
 
-        return jsonify(
-            {
-                "code": 500,
-                "message": "An error occured while creating the ticket. " +str(e)
-            }
-        ), 500
+#         return jsonify(
+#             {
+#                 "code": 500,
+#                 "message": "An error occured while creating the ticket. " +str(e)
+#             }
+#         ), 500
     
-    print(json.dumps(ticket.json()), default=str)
+#     print(json.dumps(ticket.json()), default=str)
 
-    return jsonify(
-        {
-            "code": 201,
-            "data": ticket.json()
-        }
-    ), 201
+#     return jsonify(
+#         {
+#             "code": 201,
+#             "data": ticket.json()
+#         }
+#     ), 201
 
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5004, debug=False)
+if __name__ == '__main__':
+    print("This is flask for " + os.path.basename(__file__) + ": manage event ...")
+    app.run(host='0.0.0.0', port=5004, debug=True)
