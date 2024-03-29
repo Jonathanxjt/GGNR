93fc2cdd593af6439a655b4d189e282f1f36c278
matthewaeria <matthewjamesaeria@gmail.com>
2024-03-29 16:08:34 +0800

    fixed navbar hover and hamburger colors

--- ggnr/src/components/MyNavbar/MyNavbar.jsx
+++ ggnr/src/components/MyNavbar/MyNavbar.jsx
@@ -13,7 +13,7 @@ export const MyNavbar = () => {
   };
   
   return (
-    <Navbar expand="lg" className="px-4 sticky-top custom-navbar" >
+    <Navbar variant='dark' expand="lg" className="px-4 sticky-top custom-navbar" >
       <Navbar.Brand href="/">GGNR.</Navbar.Brand>
       <Navbar.Toggle aria-controls="basic-navbar-nav" />
       <Navbar.Collapse id="basic-navbar-nav">
@@ -25,7 +25,7 @@ export const MyNavbar = () => {
         <Nav.Link href="/login" style={profileIconStyle}>
           <BsPerson />
         </Nav.Link>
-      </Navbar.Collapse>
+      </Navbar.Collapse >
     </Navbar>
   );
 };
