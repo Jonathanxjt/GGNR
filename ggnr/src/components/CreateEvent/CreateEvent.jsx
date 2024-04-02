2f889766955fcbaf704769e2b2728233a5acbb5e
matthewaeria <matthewjamesaeria@gmail.com>
2024-04-02 23:36:32 +0800

    merge conflicts

--- ggnr/src/components/CreateEvent/CreateEvent.jsx
+++ ggnr/src/components/CreateEvent/CreateEvent.jsx
@@ -78,55 +78,6 @@ const CreateEvent = () => {
 		}
 	};
 
-<<<<<<< HEAD
-	const handleSubmit = async (e) => {
-		// Handle form submission
-		e.preventDefault();
-		// Construct the EventTypes array based on the selected tiers and their capacities
-		const eventTypes = [];
-		if (selectedTiers.includes("Free Entry")) {
-			// Check if Free Entry is selected
-			eventTypes.push({
-				TierID: 1,
-				Category: "Audience",
-				Price: 0,
-				Capacity: parseInt(freeEntryCapacity, 10),
-				PriceID: null,
-			});
-		}
-		if (selectedTiers.includes("VIP")) {
-			// Check if VIP is selected
-			eventTypes.push({
-				TierID: 2,
-				Category: "VIP",
-				Price: 10,
-				Capacity: parseInt(vipCapacity, 10),
-				PriceID: "price_1OxkTz2LfOffQtXe2Amrcy0e",
-			});
-		}
-		if (selectedTiers.includes("Competitor")) {
-			// Check if Competitor is selected
-			eventTypes.push({
-				TierID: 3,
-				Category: "Competitor",
-				Price: 15,
-				Capacity: parseInt(competitorCapacity, 10),
-				PriceID: "price_1OxkUn2LfOffQtXeGLBwfvyu",
-			});
-		}
-
-		const eventData = {
-			Title: title,
-			Description: description,
-			EventLogo: eventLogo,
-			GameName: gameName,
-			GameLogo: gameLogo,
-			Location: location,
-			Time: `${date} ${time}`,
-			organiser_company: organiser_company,
-			EventTypes: eventTypes,
-		};
-=======
   const handleSubmit = async (e) => {
     e.preventDefault();
     // Construct the EventTypes array based on the selected tiers and their capacities
@@ -173,7 +124,6 @@ const CreateEvent = () => {
       EventTypes: eventTypes
     };
     console.log('Submitting event data:', eventData); // Print the JSON data
->>>>>>> c073f794a0d1d0a93051c660fc13668d51bae78f
 
 		try {
 			const response = await axios.post(
@@ -198,105 +148,7 @@ const CreateEvent = () => {
 				transition: Flip,
 			});
 
-<<<<<<< HEAD
-			// Handle error (e.g., show an error message)
-		}
-	};
-
-	return (
-		<div>
-			<MyNavbar />
-			<div className="container">
-				<Form onSubmit={handleSubmit}>
-					<Form.Group controlId="title">
-						<Form.Label>Title</Form.Label>
-						<Form.Control
-							type="text"
-							placeholder="Enter title"
-							value={title}
-							onChange={(e) => setTitle(e.target.value)}
-						/>
-					</Form.Group>
-
-					<Form.Group controlId="description">
-						<Form.Label>Description</Form.Label>
-						<Form.Control
-							as="textarea"
-							rows={3}
-							placeholder="Enter description"
-							value={description}
-							onChange={(e) => setDescription(e.target.value)}
-						/>
-					</Form.Group>
-					<Row className="g-2">
-						<Col md>
-							<Form.Group controlId="date">
-								<Form.Label>Date</Form.Label>
-								<Form.Control
-									type="date"
-									placeholder="Enter date"
-									value={date}
-									onChange={(e) => setDate(e.target.value)}
-								/>
-							</Form.Group>
-						</Col>
-						<Col md>
-							<Form.Group controlId="time">
-								<Form.Label>Time</Form.Label>
-								<Form.Control
-									type="time"
-									placeholder="Select time"
-									value={time}
-									onChange={(e) => setTime(e.target.value)}
-								/>
-							</Form.Group>
-						</Col>
-					</Row>
-
-					<Form.Group controlId="location">
-						<Form.Label>Location</Form.Label>
-						<Form.Control
-							type="text"
-							placeholder="Enter location"
-							value={location}
-							onChange={(e) => setLocation(e.target.value)}
-						/>
-					</Form.Group>
-
-					<Form.Group controlId="image">
-						<Form.Label>Upload Image</Form.Label>
-						<Form.Control
-							type="file"
-							accept="image/*"
-							onChange={handleImageChange}
-						/>
-						{previewImage && (
-							<img
-								src={previewImage}
-								alt="Preview"
-								style={{ marginTop: "10px", width: "300px", height: "300px" }}
-							/>
-						)}
-					</Form.Group>
-=======
-    } catch (error) {
-      console.log("Error creating event:", eventData);
-      console.error("Error creating event:", error);
-      console.error("Event creation error:", error.response.data.message);
-      toast.error("Event Creation Failed!", {
-        position: "top-center",
-        autoClose: 3000,
-        hideProgressBar: false,
-        closeOnClick: true,
-        pauseOnHover: true,
-        draggable: true,
-        progress: undefined,
-        theme: "dark",
-        transition: Flip,
-        });
-      
-      // Handle error (e.g., show an error message)
-    }
+    } 
   };
 
   return (
@@ -380,7 +232,6 @@ const CreateEvent = () => {
               />
             )}
           </Form.Group>
->>>>>>> c073f794a0d1d0a93051c660fc13668d51bae78f
 
 					<Form.Group>
 						<Form.Label>Select Ticket Tiers</Form.Label>
@@ -436,19 +287,6 @@ const CreateEvent = () => {
 						)}
 					</Form.Group>
 
-<<<<<<< HEAD
-					<Form.Group controlId="gameName" className="mt-2">
-						<Form.Label>Game Name</Form.Label>
-						<Form.Control
-							type="text"
-							placeholder="Enter game name"
-							value={gameName}
-							onChange={(e) => setGameName(e.target.value)}
-							onKeyDown={handleKeyDown}
-						/>
-						{isLoading && <div style={{ height: "50px", width: "100%" }}></div>}
-						{isLoading && <div className="custom-loader"></div>}
-=======
           <Form.Group controlId="gameName" className="mt-2">
             <Form.Label>Game Name</Form.Label>
             <Form.Control
@@ -461,51 +299,11 @@ const CreateEvent = () => {
             />
             {isLoading && <div style={{ height: "50px", width: "100%" }}></div>}
             {isLoading && <div className="custom-loader"></div>}
->>>>>>> c073f794a0d1d0a93051c660fc13668d51bae78f
 
 						{isLoading && (
 							<div style={{ height: "750px", width: "100%" }}></div>
 						)}
 
-<<<<<<< HEAD
-						{showDropdown && gameResults.length > 0 && (
-							<ul
-								style={{ listStyleType: "none", padding: "10px 0", margin: 0 }}
-							>
-								{gameResults.map((game) => (
-									<li
-										key={game.id}
-										style={{ cursor: "pointer", padding: "5px" }}
-										onClick={() => {
-											setGameName(game.name); // Update gameName with the selected game
-											setGameLogo(game.cover_url); // Update gameLogo with the selected game's cover URL
-											setShowDropdown(false); // Hide the dropdown
-										}}
-									>
-										<img
-											src={game.cover_url} // Use the .cover_url property for the image source
-											alt={game.name}
-											style={{
-												width: "150px",
-												height: "150px",
-												marginRight: "10px",
-											}} // Set the size of the thumbnail
-										/>
-										{game.name}
-									</li>
-								))}
-							</ul>
-						)}
-					</Form.Group>
-					<div> </div>
-					<Button variant="primary" type="submit" className="mt-3">
-						Create Event
-					</Button>
-				</Form>
-			</div>
-		</div>
-	);
-=======
             {showDropdown && gameResults.length > 0 && (
               <ul
                 style={{ listStyleType: "none", padding: "10px 0", margin: 0 }}
@@ -545,7 +343,6 @@ const CreateEvent = () => {
       <MyFooter />
     </div>
   );
->>>>>>> c073f794a0d1d0a93051c660fc13668d51bae78f
 };
 
 export default CreateEvent;
